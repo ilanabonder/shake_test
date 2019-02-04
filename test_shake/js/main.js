38ef3aa4e837916e21e28de98ddd071ca073dcc5
Ilana Bonder <ilanabonder@users.noreply.github.com>
2019-02-04 17:05:03 -0500

    Update main.js

--- test_shake/js/main.js
+++ test_shake/js/main.js
@@ -55,7 +55,7 @@ function shakePage(){
 }
 
 let myShakeEvent = new Shake({
-    threshold: 50, // optional shake strength threshold
+    threshold: 15, // optional shake strength threshold
     timeout: 1000 // optional, determines the frequency of event generation
 });
 
